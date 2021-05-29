8f4d289a47933d4e73e32a4ec8e82babecdbff11
AEKalinin <alex_kae@mail.ru>
2021-05-29 14:22:56 +0300

    HomePage4_6

--- src/components/BasePaginattion.vue
+++ src/components/BasePaginattion.vue
@@ -1,24 +1,24 @@
 <template>
   <ul class="catalog__pagination pagination">
     <li class="pagination__item">
-      <a class="pagination__link pagination__link--arrow" href="#"
-         aria-label="Предыдущая страница" v-show="page !== 1"
-         @click.prevent="paginate(page-1)">
+      <a :class="pageLink(1)" href="#"
+         aria-label="Предыдущая страница"
+         @click.prevent="paginate(page-1, 1)" >
         <svg width="8" height="14" fill="currentColor">
           <use xlink:href="#icon-arrow-left"></use>
         </svg>
       </a>
     </li>
     <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
-      <a class="pagination__link" @click.prevent="paginate(pageNumber)"
+      <a class="pagination__link" @click.prevent="paginate(pageNumber, 0)"
          :class="{'pagination__link--current': pageNumber === page}"  href="#">
         {{ pageNumber }}
       </a>
     </li>
     <li class="pagination__item">
-      <a class="pagination__link pagination__link--arrow" href="#"
-         aria-label="Следующая страница" v-show="page !==pages"
-         @click.prevent="paginate(page+1)">
+      <a :class="pageLink(2)" href="#"
+         aria-label="Следующая страница"
+         @click.prevent="paginate(page+1,2)">
         <svg width="8" height="14" fill="currentColor">
           <use xlink:href="#icon-arrow-right"></use>
         </svg>
@@ -33,15 +33,40 @@ export default {
     prop: 'page',
     event: 'paginate',
   },
-  props: ['page', 'count', 'perPage'],
+  props: ['page', 'count', 'perPage', 'isItemPage'],
   computed: {
     pages() {
       return Math.ceil(this.count / this.perPage);
     },
   },
   methods: {
-    paginate(page) {
-      this.$emit('paginate', page);
+    pageLink(isItemPage) {
+      let lnk;
+      if (this.enablePrev(isItemPage)) {
+        lnk = 'pagination__link pagination__link--arrow';
+      } else {
+        lnk = 'pagination__link pagination__link--disabled';
+      }
+      return lnk;
+    },
+    enablePrev(isItemPage) {
+      let isEnable;
+      isEnable = 0;
+      if (isItemPage === 0) {
+        isEnable = 1;
+      }
+      if (isItemPage === 1 && this.page !== 1) {
+        isEnable = 1;
+      }
+      if (isItemPage === 2 && this.page !== this.pages) {
+        isEnable = 1;
+      }
+      return isEnable === 1;
+    },
+    paginate(page, isItemPage) {
+      if (this.enablePrev(isItemPage)) {
+        this.$emit('paginate', page);
+      }
     },
   },
 };
